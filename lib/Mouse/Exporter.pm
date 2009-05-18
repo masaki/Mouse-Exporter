843f1c5627389146d03c7e890ff9bbce2702e8e2
NAKAGAWA Masaki <masaki.nakagawa@gmail.com>
2009-05-19 08:58:03 +0900

    implements

--- lib/Mouse/Exporter.pm
+++ lib/Mouse/Exporter.pm
@@ -1,9 +1,93 @@
 package Mouse::Exporter;
 
 use 5.008_001;
-use Mouse;
+use strict;
+use warnings;
+use Mouse ();
+use Data::Util ();
 
 our $VERSION = '0.01';
+
+sub import {
+    strict->import;
+    warnings->import;
+}
+
+sub setup_import_methods {
+    my ($class, %args) = @_;
+
+    $args{exporting_package} ||= caller;
+    my ($import, $unimport) = $class->build_import_methods(%args);
+
+    no strict 'refs';
+    *{ $args{exporting_package} . '::import'   } = $import;
+    *{ $args{exporting_package} . '::unimport' } = $unimport;
+}
+
+sub build_import_methods {
+    my ($class, %args) = @_;
+
+    my $caller = $args{exporting_package} || caller;
+    my (%exports, %unimports);
+
+    for my $name (@{ $args{with_caller} || [] }) {
+        my $code = do {
+            no strict 'refs';
+            \&{ $caller . '::' . $name };
+        };
+
+        $exports{$name} = $code;
+        $unimports{$name}++;
+    }
+
+    for my $name (@{ $args{as_is} || [] }) {
+        my $code;
+
+        if (ref $name) {
+            $code = $name;
+
+            my $package;
+            ($package, $name) = Data::Util::get_code_info($name);
+
+            $unimports{$name}++ if $package eq $caller;
+        }
+        else {
+            $code = do {
+                no strict 'refs';
+                \&{ $caller . '::' . $name };
+            };
+
+            $unimports{$name}++;
+        }
+
+        $exports{$name} = $code;
+    }
+
+    my @packages = ref $args{also} ? @{ $args{also} } : ($args{also});
+
+    return $class->_make_import_methods(
+        exports   => \%exports,
+        unimports => \%unimports,
+        packages  => \@packages,
+    );
+}
+
+sub _make_import_methods {
+    my ($class, %args) = @_;
+
+    my $import = sub {
+        strict->import;
+        warnings->import;
+
+        # TODO: not implemented yet
+    };
+
+    my $unimport = sub {
+        # TODO: not implemented yet
+    };
+
+    return ($import, $unimport);
+}
 
 1;
 
